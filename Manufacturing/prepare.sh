3ab5f4ddeeeba741cf7bd6068b9b1cd05f8a78b0
Thomas Mørch <thomas@bowmo.dk>
2015-07-28 23:46:44 +0200

    Added preparation script to git

---
+++ Manufacturing/prepare.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+rm dirtypcb.zip
+
+orgname=(B_Cu B_Mask B_SilkS F_Cu F_Mask F_SilkS Edge_Cuts)
+newname=(GBL GBS GBO GTL GTS GTO GBR)
+
+for item in ${!orgname[*]}
+do 
+	file=$(echo *-${orgname[$item]}.gbr)
+	r=$(echo -${orgname[$item]}.gbr)
+	n=$(echo .${newname[$item]})
+	file2=${file/$r/$n}
+	cp  $file $file2
+done
+
+drl=$(echo *.drl)
+drl=${drl/.drl/}
+
+cp $drl.drl drl.TXT
+
+
+zip -m dirtypcb.zip *.GB? *.GT? *.TXT
